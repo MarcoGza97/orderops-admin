75d9cf489bbf7d887eb47e5315ea5757b8442caa
MarcoGza97 <marco_garza.97@hotmail.com>
2026-06-23 18:29:06 -0600

    Configure EF Core and SQL Server

---
+++ OrderOps.Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderOps.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderOps.Infrastructure.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.Property(x => x.Status)
+                .IsRequired()
+                .HasConversion<int>();
+
+            builder.HasOne(x => x.Customer)
+                .WithMany(x => x.Orders)
+                .HasForeignKey(x => x.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ OrderOps.Infrastructure/Data/OrderOpsDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderOps.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderOps.Infrastructure.Data
+{
+    public class OrderOpsDbContext(DbContextOptions<OrderOpsDbContext> options) : DbContext(options)
+    {
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrderOpsDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
